fb8565470233a2ac351135ebb8ddf4e8cf767e9d
Yeomin Yim <2022110326@dgu.ac.kr>
2026-04-08 19:26:20 +0900

    2026-04-08 개인과제6 5번

--- subject5/main.java
+++ subject5/main.java
@@ -13,10 +13,17 @@ public class Main {
         System.out.print("학과를 입력하세요: ");
         String major = scanner.nextLine();
 
+        System.out.print("학교를 입력하세요: ");
+        String uni = scanner.nextLine();
+
+        System.out.print("학년을 입력하세요: ");
+        String year = scanner.nextLine();
+
         System.out.println("\n<출력>");
         System.out.println("이름: " + name);
         System.out.println("학번 : " + studentId);
         System.out.println("학과: " + major);
+        System.out.println("학년: " + year);
 
         scanner.close();
     }
