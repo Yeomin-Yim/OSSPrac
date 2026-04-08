c0b179b553cc3a8e48f819e3187b7896aa2b0c72
Yeomin Yim <2022110326@dgu.ac.kr>
2026-04-08 16:06:36 +0900

    2026-04-08 개인과제5 3번

---
+++ subject5/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("이름을 입력하세요: ");
+        String name = scanner.nextLine();
+
+        System.out.print("학번을 입력하세요: ");
+        String studentId = scanner.nextLine();
+
+        System.out.println("\n<출력>");
+        System.out.println("이름: " + name);
+        System.out.println("학번 : " + studentId);
+
+        scanner.close();
+    }
+}
